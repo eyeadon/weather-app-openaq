be725f233c1834dcb2631f1464e36fce905d8a2e
eyeadon <eyeadon40@gmail.com>
2025-07-29 15:31:56 -0400

    updating to openaq v3

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const express = require('express');
-const Datastore = require('nedb');
-const fetch = require('node-fetch');
-require('dotenv').config();
+const express = require("express");
+const Datastore = require("nedb");
+const fetch = require("node-fetch");
+require("dotenv").config();
 // console.log(process.env);
 
 const app = express();
 const port = process.env.PORT || 3000;
 // in browser, must use localhost:3000, not GoLive
 app.listen(port, () => console.log(`listening at ${port}`));
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // JSON parser, understand incoming data as JSON
 // request object updated with new body object containing parsed data
-app.use(express.json({ limit: '1mb'}));
+app.use(express.json({ limit: "1mb" }));
 
-const database = new Datastore('database.db');
+const database = new Datastore("database.db");
 database.loadDatabase();
 
-app.get('/api', (req, res) => {
+app.get("/api", (req, res) => {
   database.find({}, (err, data) => {
     if (err) {
       res.end();
@@ -27,12 +27,12 @@ app.get('/api', (req, res) => {
   });
 });
 
-app.post('/api', (req, res) => {
-  console.log('request received');
+app.post("/api", (req, res) => {
+  console.log("request received");
   // console.log(req.body);
   // JSON string is received from client, parsed into object
   const data = req.body;
-  
+
   const timestamp = Date.now();
   data.timestamp = timestamp;
 
@@ -52,31 +52,33 @@ app.post('/api', (req, res) => {
   // });
 });
 
-
 // must install node-fetch?
 
-app.get('/weather/:latlon', async (req, res) => {
+app.get("/weather/:latlon", async (req, res) => {
   // route parameters
-  const latlon = req.params.latlon.split(',');
+  const latlon = req.params.latlon.split(",");
+
   // const lat = latlon[0];
   // const lon = latlon[1];
+
   // Boston
   const lat = 42.36;
   const lon = -71.0588;
-  
+
   const API_KEY_WEATHERMAP = process.env.API_KEY_WEATHERMAP;
   const weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${API_KEY_WEATHERMAP}`;
   const weatherResponse = await fetch(weatherURL);
   const weatherData = await weatherResponse.json();
 
   const API_KEY_OPENAQ = process.env.API_KEY_OPENAQ;
-  const aqURL = `https://api.openaq.org/v2/latest?coordinates=${lat},${lon}`;
+  const aqURL = `https://api.openaq.org/v3/locations?coordinates=${lat},${lon}&radius=12000&limit=1000`;
+
   // request options, request must use valid body data type
   const options = {
-    method: 'GET',
+    method: "GET",
     headers: {
-      'Content-Type': 'application/json',
-      'X-API-Key': API_KEY_OPENAQ
+      "Content-Type": "application/json",
+      "X-API-Key": API_KEY_OPENAQ,
     },
   };
   const aqResponse = await fetch(aqURL, options);
@@ -84,9 +86,29 @@ app.get('/weather/:latlon', async (req, res) => {
 
   const atmosphereData = {
     weather: weatherData,
-    air_quality: aqData
+    air_quality: aqData,
   };
   res.json(atmosphereData);
 });
 
+app.get("/air_quality/:sensorId", async (req, res) => {
+  const sensorId = req.params.sensorId;
 
+  const API_KEY_OPENAQ = process.env.API_KEY_OPENAQ;
+  const aqMeasurementsURL = `https://api.openaq.org/v3/sensors/${sensorId}`;
+
+  const options = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "X-API-Key": API_KEY_OPENAQ,
+    },
+  };
+  const response = await fetch(aqMeasurementsURL, options);
+  const data = await response.json();
+
+  const airQualityData = {
+    measurements: data,
+  };
+  res.json(airQualityData);
+});
